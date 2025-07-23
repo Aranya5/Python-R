153e2b92e8629b73addd4a54cb14cb5bd12d4b1f
Aranya5 <aranyabasu5@gmail.com>
2025-07-23 21:35:22 +0530

    Day one of Python progarm

---
+++ day1/receipt.py
@@ -0,0 +1,13 @@
+a = int(input("Enter number of pizzas: "))
+b = int(input("Enter number of puffs: "))
+c = int(input("Enter number of drinks: "))
+
+x = a * 100
+y = b * 20
+z = c * 10
+total = x + y + z
+
+print("Amout of pizzas: ",a,"X 100 =", x)
+print("Amount of puffs: ",b,"X 20 =", y)
+print("Amount of drinks: ",c,"X 10 =", z)
+print("Total amount: ", total)
